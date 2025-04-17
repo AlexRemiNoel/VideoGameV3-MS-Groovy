180cfd99f05795dfe12733b5302bc58c07b3132f
AlexAleno <sasha.f.noel@gmail.com>
2025-04-17 09:31:08 -0400

    maps games

--- game-management/src/main/java/com/champsoft/gamemanagement/DataMapper/GameRequestMapper.java
+++ game-management/src/main/java/com/champsoft/gamemanagement/DataMapper/GameRequestMapper.java
@@ -12,14 +12,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",imports = {UUID.class, GameRequestModel.class, GameId.class, LocalDateTime.class, Genre.class})
 public interface GameRequestMapper {
-    @Mapping(target = "gameId", ignore = true)
-    @Mapping(target = "releaseDate", ignore = true)
-    @Mapping(target = "genre", ignore = true)
-//    @Mapping(target = "gameId", expression = "java(new GameId(UUID.randomUUID().toString()))")
-//    @Mapping(target = "releaseDate", expression = "java(LocalDateTime.now())")
-//    @Mapping(target = "genre", expression = "java(Genre.valueOf(gameRequestModel.getGenre()))")
+//    @Mapping(target = "gameId", ignore = true)
+//    @Mapping(target = "releaseDate", ignore = true)
+//    @Mapping(target = "genre", ignore = true)
+    @Mapping(target = "gameId", expression = "java(new GameId(UUID.randomUUID().toString()))")
+    @Mapping(target = "releaseDate", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "genre", expression = "java(Genre.valueOf(gameRequestModel.getGenre()))")
     Game requestModelToEntity(GameRequestModel gameRequestModel);
 //    List<Game> requestModelToEntity(List<GameRequestModel> gameRequestModels);
 }
