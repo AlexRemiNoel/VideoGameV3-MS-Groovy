81302948ddc4ccec63b4a37bf5874e3ec5402a23
jurassicjey <jurassicjey@gmail.com>
2025-04-19 12:57:10 -0400

    DOWNLOAD API GATEWAY STABLE CONTRCAIENRS RUNNING

---
+++ api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+
+
+		ConfigurableApplicationContext context = SpringApplication.run(ApiGatewayServiceApplication.class, args);
+		System.out.println("Active Profile(s):" + String.join(",", context.getEnvironment().getActiveProfiles()));
+	}
+
+}
